aa64f205ed788c55eaa255ed1f85f9f379fb525e
Alex Israels <Alex.Israels@Amway.com>
2021-12-30 12:53:52 -0500

    f - producer with keys

---
+++ src/main/java/com/github/ajisrael/tutorial1/ProducerDemoKeys.java
@@ -0,0 +1,64 @@
+package com.github.ajisrael.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoKeys {
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // Create Producer properties
+        Properties properties = new Properties();
+
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // Loop for generating some data
+        for (int i = 0; i < 10; i++) {
+
+            String topic = "first_topic";
+            String key = "id_" + Integer.toString(i);
+            String value = "Hello world " + Integer.toString(i);
+
+            // Create producer record - a key's hash determines which partition the record goes to
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
+
+            // Send data - asynchronous
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        // the record was successfully sent
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp() + "\n"
+                        );
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            });
+
+        }
+
+        // flush data
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
+    }
+}
